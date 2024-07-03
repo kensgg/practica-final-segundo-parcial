624bef7d72ff2865ce6a93f4c9cb956d34496a78
kensgg <g03.gudicruz@gmail.com>
2024-07-03 11:56:31 -0600

    Cambios funsionados

--- bd/conexionBD.js
+++ bd/conexionBD.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 class ConectarBD {
     constructor() {
     this.conexion = null;
@@ -6,11 +7,11 @@ class ConectarBD {
     async conectarMySQL() {
     try {
         this.conexion=await this.mysql.createConnection({
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "tiendaUT",
-        port: "3306",
+        host:process.env.HOSTMYSQL,
+        user:process.env.USERMYSQL,
+        password:process.env.PASSWORDMYSQL,
+        database:process.env.DATABASEMYSQL,
+        port:process.env.PORTMYSQL
         });
         console.log("Conexion creada a MySql");
     } catch (error) {
